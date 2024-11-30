d58b813b6f2482d74b55e4f37f9db18b51c455ba
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2024-11-30 13:10:50 +0000

    App.js is updated

--- Practical01/myapp/src/App.js
+++ Practical01/myapp/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
+import AboutUs from './components/AboutUs';
+import Contactus from './components/Contactus';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+function App(){
+
+    return(
+        <div className="App">
+            <Router>
+                <nav>
+                    <ul> 
+                        <li><Link to="/home">Home</Link></li>   
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+
+{/*Route path*/}
+                <Routes>
+                    <Route path='/home' element={<Home/>}/>
+                    <Route path='/about' element={<AboutUs/>}/>
+                    <Route path='/contact' element={<Contactus/>}/>
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
+
