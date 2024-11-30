1b0b553a50cb13cefdaf74afd6a80ed156d51d2e
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2024-11-30 14:37:04 +0000

    some styles are added

--- Practical02/react_navigation/src/components/Dashboard.js
+++ Practical02/react_navigation/src/components/Dashboard.js
@@ -1,7 +1,15 @@
-export default function Dashboard(){
-    return(
-        <div>
-            <h1>Hi, Welcome to Dashboard</h1>
+import { NavLink } from "react-router-dom";
+
+export default function Dashboard() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>Welcome to the Dashboard!</h1>
+            <nav>
+                <NavLink to="/" style={{ marginRight: "10px" }}>
+                    Login
+                </NavLink>
+                <NavLink to="/dash">Dashboard</NavLink>
+            </nav>
         </div>
-    )
+    );
 }
